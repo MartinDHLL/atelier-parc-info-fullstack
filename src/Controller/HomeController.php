d1ca31f1561e31f1605bc0cee90e1c6fe7731a67
Martin Dhollande <martin.dhollande@outlook.fr>
2023-03-28 08:56:35 +0200

    mise en place de l'authentification

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,10 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route(path:"/", name:"app_")]
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[IsGranted("ROLE_TECH")]
+    #[Route('/home', name: 'home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
